88a93b2111c27fbd197b188ebddb2fa1910a8ebf
Divija Parwatala <divijaparwatala@gmail.com>
2026-07-13 20:15:08 +0530

    Frontend URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,7 @@ console.log("ENV:",
     import.meta.env.VITE_API_URL);
 const api = axios.create({
     baseURL: (
-        import.meta.env.VITE_API_URL || "http://localhost:8080") + "/api",
+        import.meta.env.VITE_API_URL ) + "/api",
     withCredentials: true,
     paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://foodgenie-ah6z.onrender.com',
         changeOrigin: true,
         secure: false,
       }
